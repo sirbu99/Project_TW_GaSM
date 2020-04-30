711168c519257edb69e8c9b1ccdcd608773b3ce1
Constantin Suruniuc <48994982+Costyyy@users.noreply.github.com>
2020-04-30 19:34:15 +0300

    minor tweak

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 dashboardbp = Blueprint('dashboardbp', __name__)
 
-dashboardbp.route('/dashboard', methods=['GET', 'POST'])
+@dashboardbp.route('/info', methods=['GET', 'POST'])
 
 
 def dashboard():
+    return render_template('index.html')
     pass
