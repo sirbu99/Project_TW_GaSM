299c025d5864a62ed202355ca51a06f2095f4279
Constantin Suruniuc <48994982+Costyyy@users.noreply.github.com>
2020-06-08 20:24:45 +0300

    added doc file

---
+++ app/views/home/doc.php
@@ -0,0 +1,77 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>GaSM Scholarly HTML</title>
+    <link rel="stylesheet" href="scholarly.css">
+</head>
+
+<body>
+<article>
+    <header>
+        <h1>GaSM</h1>
+    </header>
+    <div>
+        <ol>
+            <li>
+                <span>Iuliana</span>
+                <span>Holban</span>
+            </li>
+            <li>
+                <span>Simona</span>
+                <span>Sîrbu</span>
+            </li>
+            <li>
+                <span>Constantin</span>
+                <span>Suruniuc</span>
+            </li>
+        </ol>
+    </div>
+    <section>
+        <h2>Abstract</h2>
+        <p>
+            Să se creeze o aplicație Web capabilă, pe baza unui API REST/GraphQL propriu, să gestioneze informațiile
+            privitoare la colectarea, sortarea și reciclarea gunoiului – pe categorii: menajer, hârtie, plastic etc.
+            – la nivelul cetățeanului, personalului autorizat și factorilor de decizie.<br>
+
+            Se va oferi suport pentru raportarea de către utilizatori a locurilor unde s-a acumulat o cantitate
+            substanțială de gunoi, în vederea descongestionării. Pe unitate de timp (zi, săptămână, lună), vor fi
+            generate rapoarte numerice și grafice – disponibile în formatele HTML, CSV și PDF – referitoare la
+            situația actuală la nivel de cartier/localitate, evidențiindu-se zonele cele mai curate/mizere.<br>
+
+            Interacțiunea cu utilizatorul va respecta principiile designului Web responsiv. Sistemul va oferi suport
+            și pentru inițierea unor campanii de sensibilizare a locuitorilor privitoare la colectarea selectivă a
+            gunoiului și a raportării incidentelor vizând depozitarea neadecvată a acestuia.<br>
+        </p>
+    </section>
+    <section>
+        <h2>Motivație</h2>
+        <p>
+            Aplicația are ca și scop gestionarea informațiilor despre colectarea și reciclarea gunoiului, procesarea
+            acestora în rapoarte și grafice utile, informarea utilizatorilor despre evenimente ecologice și preluarea
+            informațiilor de la utilizatori despre cantități excesive de gunoi și colectarea necorespunzătoare a
+            gunoiului.
+        </p>
+    </section>
+    <section>
+        <h2>Structură</h2>
+        <ul>
+            <li>Pagină de login/register</li>
+            <li>Pagina principală care conține:</li>
+            <ul>
+                <li>Listă cu evenimente</li>
+                <li>Informații despre reciclare</li>
+                <li>Hartă pentru raportarea de probleme</li>
+            </ul>
+            <li>Pagină pentru vizualizarea și descărcarea de statistici</li>
+            <li>API pentru inserarea și preluarea de informații</li>
+        </ul>
+        <section>
+            <h2>Login/Register</h2>
+            
+        </section>
+    </section>
+</article>
+
+</body>
